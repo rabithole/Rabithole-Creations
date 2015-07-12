db08a2947ada6de6ce343ee401d2cf79719b413a
rabithole <pedalinon@gmail.com>
2015-07-11 22:02:47 -0700

    Working slideshow for section one, still need to create fade in and out.

--- scripts/sec_1_images.js
+++ scripts/sec_1_images.js
@@ -18,28 +18,26 @@ request.onreadystatechange = function() {
 		var img =  JSON.parse(request.responseText);
 		console.log(img);
 
-		var output = "";
-		output += section_1.style.backgroundImage = "url('" + img.img[0].url + "')";
-		section_1.innerHTML = output + "<img src='images/backgrounds/rabithole_logo.png'><br>";
-		section_2.innerHTML = "<h1>Hello World!</h1><br>" + "<h5>Here I Come</h5>";
-		
-		var i = 0;
+		function fadeOut() {
+			section_1.className = "fadeOut";
+		}
 
-		
-		function stepThrough() {		
+		function fadeIn() {
+				section_1.className = "fadeIn";
+			}
+
+		var i = 0;		
+		function stepThrough() {
+		section_2.innerHTML = "<h1>Hello World!</h1><br>" + "<h2>Here I Come</h2>";	
 			setInterval(function() {
 				if (i < img.img.length) {
-			    	var output = "";
-					//section_1.style.backgroundImage = "url(" + img.img[i].url + ")";
-					section_1.style.cssText = 
-						"background-image:url(" + img.img[i].url + "); /*opacity: 0; transition-property: opacity; transition-duration: 2s;*/";
-					section_1.innerHTML = output + "<img src='images/backgrounds/rabithole_logo.png'><br>";
-					section_2.innerHTML = "<h1>Hello World!</h1><br>" + "<h5>Here I Come</h5>";
-					console.log("Here");
-					i += 1;
+					section_1.style.cssText = "background-image: url(" + img.img[i].url + ");";
+					
+					console.log("Here");	
+					i += 1;	
 				} else {
 					i = 0;
-				}
+					} 
 			}, 5000);
 		} 
 		stepThrough();
