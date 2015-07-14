3a1a67af374d9616fea9fdb8d624e1ecc9fd1c72
rabithole <pedalinon@gmail.com>
2015-07-14 16:55:46 -0700

    Landing Page update, 7-14-2014

---
+++ scripts/mobile.js
@@ -0,0 +1,12 @@
+(function() {
+	var x = screen.width;
+	console.log(x);
+
+	var head = document.getElementsByTagName("head");
+	console.log(head);
+	head[0] += "<link href='css/rabit_2.css" rel="stylesheet'>";	
+
+	if(x < 430) {
+
+	}
+})();
