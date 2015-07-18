19824b04d87d450d0f227ea76abacd92c9c8b24a
rabithole <pedalinon@gmail.com>
2015-07-17 19:57:48 -0700

    Nav scrolling effect

--- scripts/scrolling.js
+++ scripts/scrolling.js
@@ -1,5 +1,11 @@
 window.addEventListener("scroll", navScroll);
 
 function navScroll() {
+	var navChange = document.getElementById("nav");
+
 	console.log("Page is scrolling")
+	navChange.style.cssText = 
+	"position: relative; left: 50px;";
+
+	var navli = document.getElementsByClassName("nav_li");
 }
