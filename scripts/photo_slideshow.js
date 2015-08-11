47df016dd0b995320a7999de7ab2d886119115d3
rabithole <pedalinon@gmail.com>
2015-08-11 16:26:29 -0700

    Ajax slider to main page

--- scripts/photo_slideshow.js
+++ scripts/photo_slideshow.js
@@ -8,23 +8,23 @@ if(window.XMLHttpRequest) {
 	request = new ActiveXObect("Microsoft.XMLHTTP");
 }
 
-request.open('GET', 'json/photo_images.json', true);
+request.open('GET', 'json/landing.json', true);
 request.onreadystatechange = function() {
 	if((request.status === 200) && (request.readyState === 4)) {
 		
 		var img =  JSON.parse(request.responseText);
 
-		
-
 		var i = 0;		
 		function stepThrough() {
 		//section_2.innerHTML = "<h1>Hello World!</h1><br>" + "<h2>Here I Come</h2>";	
-		var photo_slide = document.getElementById("photo_slide");
-		photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";
+		var photo_slide = document.getElementById("main_page_slide");
+		//photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";
+		photo_slide.src = img.img[i].url;
 		i += 1;
 			setInterval(function() {
 				if (i < img.img.length) {
-					photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";			
+					//photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";	
+					photo_slide.src = img.img[i].url;	
 					console.log("Here");	
 					i += 1;	
 				} else {
