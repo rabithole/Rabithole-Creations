0d483da45d2a8b471749d9ea75703b2a6cb7cab3
rabithole <pedalinon@gmail.com>
2015-08-12 11:59:52 -0700

    Main page slide shows

--- scripts/photo_slideshow.js
+++ scripts/photo_slideshow.js
@@ -25,12 +25,50 @@ request.onreadystatechange = function() {
 				if (i < img.img.length) {
 					//photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";	
 					photo_slide.src = img.img[i].url;	
-					console.log("Here");	
 					i += 1;	
 				} else {
 					i = 0;
 					} 
-			}, 5000);
+			}, 3000);
+		} 
+		stepThrough();
+	}
+}
+request.send();
+})();
+
+(function() {
+	var request;
+/* This is for older browser compatability */
+
+if(window.XMLHttpRequest) {
+	request = new XMLHttpRequest();
+} else {
+	request = new ActiveXObect("Microsoft.XMLHTTP");
+}
+
+request.open('GET', 'json/img_2.json', true);
+request.onreadystatechange = function() {
+	if((request.status === 200) && (request.readyState === 4)) {
+		
+		var img =  JSON.parse(request.responseText);
+
+		var i = 0;		
+		function stepThrough() {
+		//section_2.innerHTML = "<h1>Hello World!</h1><br>" + "<h2>Here I Come</h2>";	
+		var photo_slide_2 = document.getElementById("main_1st_slide");
+		//photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";
+		photo_slide_2.src = img.img[i].url;
+		i += 1;
+			setInterval(function() {
+				if (i < img.img.length) {
+					//photo_slide.style.cssText = "background-image: url(" + img.img[i].url + ");";	
+					photo_slide_2.src = img.img[i].url;	
+					i += 1;	
+				} else {
+					i = 0;
+					} 
+			}, 3000);
 		} 
 		stepThrough();
 	}
