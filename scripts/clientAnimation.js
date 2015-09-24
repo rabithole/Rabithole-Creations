75653175d013d2e1b7d30ab58aaca266c91b6409
rabithole <pedalinon@gmail.com>
2015-09-24 14:16:02 -0700

    Client page without responsive framework

--- scripts/clientAnimation.js
+++ scripts/clientAnimation.js
@@ -8,11 +8,17 @@ $(document).ready(function() {
 		$("a").unbind("click");
 		$("ul").animate({
 			 	width: "230px",
-			 	fontSize: "1em"
+			 	fontSize: "1em",
+			 	left: "70%",
+			 	top: "300px"
 			 });
 			$("li").animate({
 				marginTop: "5px"
 			});	
+	});
+
+	$("#sec_1_h1").click(function() {
+		$("#the_process").slideToggle("slow");
 	});
 
 	// Top of page animations for everything but mobile. 
@@ -30,85 +36,85 @@ $(document).ready(function() {
 		}
 		this.timeoutId = window.setTimeout(function() {
 			if(scroll > 201) {
-				$("header").css(
-						"height", "40px",
-						"fontSize" "1em"
-					);
+				$("header").css({
+					"height": "40px",
+					"fontSize": "1em"
+				});
 
-					$("#main_title").animate({
-						fontSize: "1.7em",
-						marginTop: "0px"
-					}, 1000);
+				$("#main_title").css({
+					"fontSize": "1.7em",
+					"marginTop": "0px"
+				});
 
-					$(".media_links").animate({
-						width: "30px"
-					}, 1000);
+				$(".media_links").css(
+					"width", "30px"
+				);
 
-					$("ul").animate({
-						width: "50px",
-						fontSize: "0em", 
-						left: "90%", 
-						top: "5%"
-					});
+				$("ul").css({
+					"width": "50px",
+					"fontSize": "0em", 
+					"left": "90%", 
+					"top": "5%"
+				});
 
-					$("li").animate({
-						marginTop: "0px"
-					});
+				$("li").css(
+					"marginTop", "0px"
+				);
 
-					$("a").click(function(event) {
-					event.preventDefault();
-					});
+				$("a").click(function(event) {
+				event.preventDefault();
+				});
 
-					$(".media").unbind('click');
+				$(".media").unbind('click');
 			}
 
 			if(scroll > 10 && scroll < 200){
-					$("header").animate({
-						height: "40px",
-						fontSize: "1em"
-					}, 1000);
+				$("header").animate({
+					height: "40px",
+					fontSize: "1em"
+				}, 1000);
 
-					$("#main_title").animate({
-						fontSize: "1.7em",
-						marginTop: "0px"
-					}, 1000);
+				$("#main_title").animate({
+					fontSize: "1.7em",
+					marginTop: "0px"
+				}, 1000);
 
-					$(".media_links").animate({
-						width: "30px"
-					}, 1000);
+				$(".media_links").animate({
+					width: "30px"
+				}, 1000);
 
-					$("ul").animate({
-						width: "50px",
-						fontSize: "0em", 
-						left: "90%", 
-						top: "5%"
-					});
+				$("ul").animate({
+					width: "50px",
+					fontSize: "0em", 
+					left: "90%", 
+					top: "5%"
+				});
 
-					$("li").animate({
-						marginTop: "0px"
-					});
+				$("li").animate({
+					marginTop: "0px"
+				});
 
-					$("a").click(function(event) {
-					event.preventDefault();
-					});
+				$("a").click(function(event) {
+				event.preventDefault();
+				});
 
-					$(".media").unbind('click');
-				}
+				$(".media").unbind('click');
+			}
 
 			if(scroll < 100) {
 				$("header").animate({
 					height: "110px",
 					fontSize: "1em"
-				}, 1000);
+				});
 
 				$("#main_title").animate({
 					fontSize: "4em",
 					marginTop: "50px"
-				}, 1000);
+				});
 
 				$(".media_links").animate({
 					width: "70px"
-				}, 1000);
+				});
 
 				$("ul").animate({
 				 	width: "230px",
